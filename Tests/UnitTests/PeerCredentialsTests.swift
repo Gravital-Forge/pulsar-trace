247bc78ff10748d03f5a42be8ae4698ec1b8c290
Mateusz Krainski <mt.krainski@gmail.com>
2026-06-09 15:59:46 -0400

    test: #require socketpair success so a failure can't close fd 0/1 (review nit)

--- Tests/UnitTests/PeerCredentialsTests.swift
+++ Tests/UnitTests/PeerCredentialsTests.swift
@@ -13,10 +13,12 @@ import Darwin
 struct PeerCredentialsTests {
 
     @Test("a socketpair peer in the same process is the same user")
-    func socketpairIsSameUser() {
+    func socketpairIsSameUser() throws {
         var fds: [Int32] = [0, 0]
         let rc = socketpair(AF_UNIX, SOCK_STREAM, 0, &fds)
-        #expect(rc == 0)
+        // #require, not #expect: if socketpair failed, the deferred
+        // close(fds[0])/close(fds[1]) would close fd 0/1 (stdin/stdout).
+        try #require(rc == 0)
         defer { close(fds[0]); close(fds[1]) }
 
         #expect(PeerCredentials.peerIsSameUser(fd: fds[0]))
